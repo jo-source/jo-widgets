fe446e0f19000861f994fe322626c68555a481bc
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2010-10-04 18:27:42 +0000

    bugfix: defaultConvrter must handle null

--- trunk/jo-widgets/jo-widgets-api/src/main/java/org/jo/widgets/api/convert/impl/internal/DefaultShortConverter.java
+++ trunk/jo-widgets/jo-widgets-api/src/main/java/org/jo/widgets/api/convert/impl/internal/DefaultShortConverter.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2010, Michael Grossmann
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the name of the jo-widgets.org nor the
- *     names of its contributors may be used to endorse or promote products
- *     derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
- * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
  * DAMAGE.
  */
 package org.jo.widgets.api.convert.impl.internal;
@@ -34,8 +34,7 @@ import org.jo.widgets.api.validation.ValidationMessage;
 import org.jo.widgets.api.validation.ValidationMessageType;
 import org.jo.widgets.api.validation.ValidationResult;
 
-public class DefaultShortConverter extends AbstractObjectStringConverter<Short>
-		implements IConverter<Short> {
+public class DefaultShortConverter extends AbstractObjectStringConverter<Short> implements IConverter<Short> {
 
 	private static final String NO_VALID_INTEGER_MESSAGE = "is no valid integer number";
 
@@ -45,22 +44,25 @@ public class DefaultShortConverter extends AbstractObjectStringConverter<Short>
 			// TODO define business logic and parse (less tolerant) with
 			// consideration of i18n
 			return Short.valueOf(Short.parseShort(string));
-		} catch (final NumberFormatException e) {
+		}
+		catch (final NumberFormatException e) {
 			return null;
 		}
 	}
 
 	@Override
 	public String convertToString(final Short value) {
-		return value.toString();
+		if (value != null) {
+			return value.toString();
+		}
+		return null;
 	}
 
 	@Override
 	public ValidationResult validate(final String text) {
 		if (text != null && !text.isEmpty()) {
 			if (convertToObject(text) == null) {
-				return new ValidationResult(ValidationMessageType.ERROR,
-						NO_VALID_INTEGER_MESSAGE);
+				return new ValidationResult(ValidationMessageType.ERROR, NO_VALID_INTEGER_MESSAGE);
 			}
 		}
 		return new ValidationResult();
@@ -71,7 +73,8 @@ public class DefaultShortConverter extends AbstractObjectStringConverter<Short>
 		if (string != null) {
 			if (!string.matches("-?[0-9]*")) {
 				return new ValidationMessage(ValidationMessageType.ERROR, "'"
-						+ string + "' could not be completed to a valid number");
+					+ string
+					+ "' could not be completed to a valid number");
 			}
 		}
 		return OkMessage.getInstance();
