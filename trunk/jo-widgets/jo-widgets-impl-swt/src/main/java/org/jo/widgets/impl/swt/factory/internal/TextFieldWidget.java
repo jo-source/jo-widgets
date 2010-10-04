7df8b6b8a3aa2404e823177ec572876ccf1c8138
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2010-10-04 19:05:11 +0000

    Bugfix: TextFieldWidget must handle null

--- trunk/jo-widgets/jo-widgets-impl-swt/src/main/java/org/jo/widgets/impl/swt/factory/internal/TextFieldWidget.java
+++ trunk/jo-widgets/jo-widgets-impl-swt/src/main/java/org/jo/widgets/impl/swt/factory/internal/TextFieldWidget.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2010, Michael Grossmann
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the name of the jo-widgets.org nor the
- *     names of its contributors may be used to endorse or promote products
- *     derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
- * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
  * DAMAGE.
  */
 package org.jo.widgets.impl.swt.factory.internal;
@@ -43,15 +43,12 @@ public class TextFieldWidget extends AbstractSwtTextInputWidget<String> {
 
 	private final boolean mandatory;
 
-	public TextFieldWidget(final IColorCache colorCache, final IWidget parent,
-			final IBaseTextFieldDescriptor<?> descriptor) {
-		super(parent, colorCache, createText(parent, descriptor), descriptor
-				.getTextInputValidator());
+	public TextFieldWidget(final IColorCache colorCache, final IWidget parent, final IBaseTextFieldDescriptor<?> descriptor) {
+		super(parent, colorCache, createText(parent, descriptor), descriptor.getTextInputValidator());
 
 		this.mandatory = descriptor.isMandatory();
 
-		final ITextInputValidator validator = descriptor
-				.getTextInputValidator();
+		final ITextInputValidator validator = descriptor.getTextInputValidator();
 
 		this.getUiReference().addVerifyListener(new VerifyListener() {
 
@@ -59,10 +56,10 @@ public class TextFieldWidget extends AbstractSwtTextInputWidget<String> {
 			public void verifyText(final VerifyEvent verifyEvent) {
 				final String newText = verifyEvent.text;
 
-				if (ValidationMessageType.OK.equals(validator
-						.isCompletableToValid(newText).getType())) {
+				if (ValidationMessageType.OK.equals(validator.isCompletableToValid(newText).getType())) {
 					verifyEvent.doit = true;
-				} else {
+				}
+				else {
 					verifyEvent.doit = false;
 				}
 			}
@@ -84,7 +81,12 @@ public class TextFieldWidget extends AbstractSwtTextInputWidget<String> {
 
 	@Override
 	public void setValue(final String content) {
-		getUiReference().setText(content);
+		if (content != null) {
+			getUiReference().setText(content);
+		}
+		else {
+			getUiReference().setText("");
+		}
 	}
 
 	@Override
@@ -99,12 +101,10 @@ public class TextFieldWidget extends AbstractSwtTextInputWidget<String> {
 
 	@Override
 	public boolean hasInput() {
-		return getUiReference().getText() != null
-				&& !getUiReference().getText().isEmpty();
+		return getUiReference().getText() != null && !getUiReference().getText().isEmpty();
 	}
 
-	private static Text createText(final IWidget parent,
-			final IBaseTextFieldDescriptor<?> descriptor) {
+	private static Text createText(final IWidget parent, final IBaseTextFieldDescriptor<?> descriptor) {
 		return new Text((Composite) parent.getUiReference(), SWT.BORDER);
 	}
 
