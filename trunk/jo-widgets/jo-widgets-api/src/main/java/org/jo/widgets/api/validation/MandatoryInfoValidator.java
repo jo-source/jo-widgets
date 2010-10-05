2c9ed2a67d9e31f67f539de2452bd916326822f4
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2010-10-05 09:32:00 +0000

    message text for MandatoryValidator must be set from outside

--- trunk/jo-widgets/jo-widgets-api/src/main/java/org/jo/widgets/api/validation/MandatoryInfoValidator.java
+++ trunk/jo-widgets/jo-widgets-api/src/main/java/org/jo/widgets/api/validation/MandatoryInfoValidator.java
@@ -9,13 +9,11 @@ import org.jo.widgets.util.Assert;
 // TODO Add use ERROR_INFO message type (must be implemented)
 public class MandatoryInfoValidator<VALIDATION_INPUT_TYPE> implements IValidator<VALIDATION_INPUT_TYPE> {
 
-	private static final String INFO_MESSAGE = "Enter input to ";
-
 	private final ValidationResult infoResult;
 
-	public MandatoryInfoValidator(final String controlName) {
-		Assert.paramNotEmpty(controlName, "controlName");
-		this.infoResult = new ValidationResult(ValidationMessageType.ERROR, INFO_MESSAGE + " '" + controlName + "' ");
+	public MandatoryInfoValidator(final String messageText) {
+		Assert.paramNotEmpty(messageText, "controlName");
+		this.infoResult = new ValidationResult(ValidationMessageType.ERROR, messageText);
 	}
 
 	@Override
