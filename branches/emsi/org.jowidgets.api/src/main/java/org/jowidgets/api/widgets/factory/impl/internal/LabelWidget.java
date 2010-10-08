b0dbcd4c60d1118c272c98af255c0e4036cbeb51
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2010-10-08 16:20:25 +0000

    using artefact compatible folder names

---
+++ branches/emsi/org.jowidgets.api/src/main/java/org/jowidgets/api/widgets/factory/impl/internal/LabelWidget.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2010, Michael Grossmann
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+package org.jowidgets.api.widgets.factory.impl.internal;
+
+import org.jowidgets.api.color.IColorConstant;
+import org.jowidgets.api.image.IImageConstant;
+import org.jowidgets.api.look.Markup;
+import org.jowidgets.api.util.ColorSettingsInvoker;
+import org.jowidgets.api.widgets.ICompositeWidget;
+import org.jowidgets.api.widgets.IIconWidget;
+import org.jowidgets.api.widgets.ILabelWidget;
+import org.jowidgets.api.widgets.ITextLabelWidget;
+import org.jowidgets.api.widgets.IWidget;
+import org.jowidgets.api.widgets.blueprint.factory.impl.BluePrintFactory;
+import org.jowidgets.api.widgets.descriptor.IIconDescriptor;
+import org.jowidgets.api.widgets.descriptor.ITextLabelDescriptor;
+import org.jowidgets.api.widgets.descriptor.base.IBaseLabelDescriptor;
+import org.jowidgets.api.widgets.layout.MigLayoutDescriptor;
+
+public class LabelWidget implements ILabelWidget {
+
+	private final IIconWidget iconWidget;
+	private final ITextLabelWidget textLabelWidget;
+	private final ICompositeWidget compositeWidget;
+
+	public LabelWidget(final ICompositeWidget compositeWidget, final IBaseLabelDescriptor<ILabelWidget> descriptor) {
+
+		super();
+
+		this.compositeWidget = compositeWidget;
+		this.compositeWidget.setLayout(new MigLayoutDescriptor("0[][]0", "0[]0"));
+
+		final BluePrintFactory bpF = new BluePrintFactory();
+
+		final IIconDescriptor iconDescriptor = bpF.icon(descriptor.getIcon());
+		this.iconWidget = compositeWidget.add(iconDescriptor, "w 0::");
+
+		final ITextLabelDescriptor textLabelDescriptor = bpF.textLabel().setDescriptor(descriptor);
+		this.textLabelWidget = compositeWidget.add(textLabelDescriptor, "");
+
+		ColorSettingsInvoker.setColors(descriptor, this);
+	}
+
+	@Override
+	public void setText(final String text) {
+		textLabelWidget.setText(text);
+	}
+
+	@Override
+	public void setToolTipText(final String text) {
+		textLabelWidget.setToolTipText(text);
+	}
+
+	@Override
+	public IWidget getParent() {
+		return compositeWidget.getParent();
+	}
+
+	@Override
+	public Object getUiReference() {
+		return compositeWidget.getUiReference();
+	}
+
+	@Override
+	public void redraw() {
+		compositeWidget.redraw();
+	}
+
+	@Override
+	public void setForegroundColor(final IColorConstant colorValue) {
+		textLabelWidget.setForegroundColor(colorValue);
+	}
+
+	@Override
+	public void setBackgroundColor(final IColorConstant colorValue) {
+		textLabelWidget.setBackgroundColor(colorValue);
+	}
+
+	@Override
+	public void setMarkup(final Markup markup) {
+		textLabelWidget.setMarkup(markup);
+	}
+
+	@Override
+	public void setIcon(final IImageConstant icon) {
+		iconWidget.setIcon(icon);
+	}
+
+}
---
+++ branches/emsi/org.jowidgets.impl.swing/src/main/java/org/jowidgets/impl/swing/widgets/SwingContainerWidget.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2010, Michael Grossmann
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+package org.jowidgets.impl.swing.widgets;
+
+import java.awt.Component;
+import java.awt.Container;
+
+import net.miginfocom.swing.MigLayout;
+
+import org.jowidgets.api.color.IColorConstant;
+import org.jowidgets.api.widgets.IChildWidget;
+import org.jowidgets.api.widgets.IContainerWidget;
+import org.jowidgets.api.widgets.IWidget;
+import org.jowidgets.api.widgets.descriptor.base.IBaseWidgetDescriptor;
+import org.jowidgets.api.widgets.factory.ICustomWidgetFactory;
+import org.jowidgets.api.widgets.factory.IGenericWidgetFactory;
+import org.jowidgets.api.widgets.factory.IWidgetFactory;
+import org.jowidgets.api.widgets.layout.ILayoutDescriptor;
+import org.jowidgets.api.widgets.layout.MigLayoutDescriptor;
+import org.jowidgets.util.Assert;
+
+public class SwingContainerWidget implements IContainerWidget {
+
+	private final IGenericWidgetFactory factory;
+	private final Container container;
+	private final SwingWidget swingWidgetDelegate;
+
+	public SwingContainerWidget(final IGenericWidgetFactory factory, final Container container) {
+
+		Assert.paramNotNull(factory, "factory");
+		//Assert.paramNotNull(container, "container");
+
+		this.factory = factory;
+		this.container = container;
+		this.swingWidgetDelegate = new SwingWidget(container);
+	}
+
+	@Override
+	public final void setLayout(final ILayoutDescriptor layoutManager) {
+		Assert.paramNotNull(layoutManager, "layoutManager");
+		if (layoutManager instanceof MigLayoutDescriptor) {
+			final MigLayoutDescriptor migLayoutManager = (MigLayoutDescriptor) layoutManager;
+			container.setLayout(new MigLayout(
+				migLayoutManager.getLayoutConstraints(),
+				migLayoutManager.getColumnConstraints(),
+				migLayoutManager.getRowConstraints()));
+		}
+		else {
+			throw new IllegalArgumentException("Layout Manager of type '"
+				+ layoutManager.getClass().getName()
+				+ "' is not supported");
+		}
+	}
+
+	@Override
+	public Container getUiReference() {
+		return container;
+	}
+
+	@Override
+	public void redraw() {
+		swingWidgetDelegate.redraw();
+	}
+
+	@Override
+	public void setForegroundColor(final IColorConstant colorValue) {
+		swingWidgetDelegate.setForegroundColor(colorValue);
+	}
+
+	@Override
+	public void setBackgroundColor(final IColorConstant colorValue) {
+		swingWidgetDelegate.setBackgroundColor(colorValue);
+	}
+
+	@Override
+	public final <WIDGET_TYPE extends IChildWidget> WIDGET_TYPE add(
+		final IBaseWidgetDescriptor<? extends WIDGET_TYPE> descriptor,
+		final Object cellConstraints) {
+
+		final WIDGET_TYPE result = factory.create(this, descriptor);
+		addToContainer(result, cellConstraints);
+		return result;
+	}
+
+	@Override
+	public final <WIDGET_TYPE extends IChildWidget> WIDGET_TYPE add(
+		final ICustomWidgetFactory<WIDGET_TYPE> customFactory,
+		final Object cellConstraints) {
+
+		final IWidgetFactory<WIDGET_TYPE, IBaseWidgetDescriptor<? extends WIDGET_TYPE>> widgetFactory = new IWidgetFactory<WIDGET_TYPE, IBaseWidgetDescriptor<? extends WIDGET_TYPE>>() {
+			@Override
+			public WIDGET_TYPE create(final IWidget parent, final IBaseWidgetDescriptor<? extends WIDGET_TYPE> descriptor) {
+				return factory.create(parent, descriptor);
+			}
+		};
+
+		final WIDGET_TYPE result = customFactory.create(this, widgetFactory);
+		addToContainer(result, cellConstraints);
+		return result;
+	}
+
+	@Override
+	public void layoutBegin() {
+		//do nothing here (swing does not flicker like swt)
+	}
+
+	@Override
+	public void layoutEnd() {
+		redraw();
+	}
+
+	@Override
+	public void removeAll() {
+		container.removeAll();
+	}
+
+	protected IGenericWidgetFactory getGenericWidgetFactory() {
+		return factory;
+	}
+
+	private void addToContainer(final IChildWidget widget, final Object cellConstraints) {
+		if (cellConstraints != null) {
+			container.add((Component) (widget.getUiReference()), cellConstraints);
+		}
+		else {
+			container.add((Component) (widget.getUiReference()));
+		}
+	}
+
+}
