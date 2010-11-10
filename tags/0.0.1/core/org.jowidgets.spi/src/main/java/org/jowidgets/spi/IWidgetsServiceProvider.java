269b4e66a4dc79603f7fca75b3681dd47798327a
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2010-11-10 01:07:09 +0000

    [maven-scm] copy for tag 0.0.1

---
+++ tags/0.0.1/core/org.jowidgets.spi/src/main/java/org/jowidgets/spi/IWidgetsServiceProvider.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2010, grossmann
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+package org.jowidgets.spi;
+
+import org.jowidgets.common.application.IApplicationRunner;
+import org.jowidgets.common.image.IImageRegistry;
+import org.jowidgets.common.threads.IUiThreadAccess;
+import org.jowidgets.spi.image.IImageHandleFactorySpi;
+
+public interface IWidgetsServiceProvider {
+
+	IImageRegistry getImageRegistry();
+
+	IImageHandleFactorySpi getImageHandleFactory();
+
+	IWidgetFactorySpi getWidgetFactory();
+
+	IUiThreadAccess createUiThreadAccess();
+
+	IApplicationRunner createApplicationRunner();
+
+	/**
+	 * Gets the UI Reference (e.g. Shell for SWT, Window for Swing, ...) of the active window.
+	 * 
+	 * @return The UI Reference of the active window or null, if now window is active
+	 */
+	Object getActiveWindowUiReference();
+
+}
