ae0db3583f0d6ee4ff0a1366be6c7242348f4b7f
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2010-11-10 00:03:22 +0000

    [maven-release-plugin]  copy for tag org.jowidgets.parent-0.0.1

---
+++ tags/org.jowidgets.parent-0.0.1/trunk/core/org.jowidgets.common/src/main/java/org/jowidgets/common/image/IImageUrlProvider.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2010 PLATH Group(R), Germany. All rights reserved.
+ * Creation date: 02.11.2010
+ */
+package org.jowidgets.common.image;
+
+import java.net.URL;
+
+import org.jowidgets.common.image.IImageConstant;
+
+public interface IImageUrlProvider extends IImageConstant {
+
+	URL getImageUrl();
+
+}
