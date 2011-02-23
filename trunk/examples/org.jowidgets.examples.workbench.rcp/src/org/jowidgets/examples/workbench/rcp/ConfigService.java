050ce319d534602364f8a7ac7afd672fc3b559e7
h.westphal@gmx.de <h.westphal@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2011-02-23 18:26:14 +0000

    add rcp workbench demo starter app

---
+++ trunk/examples/org.jowidgets.examples.workbench.rcp/src/org/jowidgets/examples/workbench/rcp/ConfigService.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2011, M. Grossmann, M. Woelker, H. Westphal
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of jo-widgets.org nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+package org.jowidgets.examples.workbench.rcp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+import org.jowidgets.workbench.api.IWorkbenchConfigurationService;
+
+public class ConfigService implements IWorkbenchConfigurationService {
+
+	private static final String CONFIG_PATH = System.getProperty("user.home")
+		+ File.separator
+		+ "."
+		+ ConfigService.class.getPackage().getName()
+		+ ".config";
+
+	@Override
+	public Serializable loadConfiguration() {
+		try {
+			final ObjectInputStream oos = new ObjectInputStream(new FileInputStream(CONFIG_PATH));
+			try {
+				return (Serializable) oos.readObject();
+			}
+			finally {
+				oos.close();
+			}
+		}
+		catch (final Exception e) {
+			System.err.println("An error occured while loading config: " + e);
+			return null;
+		}
+	}
+
+	@Override
+	public void saveConfiguration(final Serializable configuration) {
+		try {
+			final ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(CONFIG_PATH));
+			try {
+				oos.writeObject(configuration);
+			}
+			finally {
+				oos.close();
+			}
+		}
+		catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
