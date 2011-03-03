c48bf3e58df046ba60e7c975b47defc5fbecea1c
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2011-03-03 13:16:22 +0000

    [maven-scm] copy for tag 0.0.15

---
+++ tags/0.0.15/core/org.jowidgets.api/src/main/java/org/jowidgets/api/model/item/IToolBarModel.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2011, grossmann
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+package org.jowidgets.api.model.item;
+
+import java.util.List;
+
+import org.jowidgets.api.command.IAction;
+import org.jowidgets.api.model.IListModelObservable;
+import org.jowidgets.common.image.IImageConstant;
+
+public interface IToolBarModel extends IListModelObservable {
+
+	void addItemsOfModel(IToolBarModel model);
+
+	<MODEL_TYPE extends IToolBarItemModel> MODEL_TYPE addItem(final MODEL_TYPE item);
+
+	<MODEL_TYPE extends IToolBarItemModel> MODEL_TYPE addItem(final int index, final MODEL_TYPE item);
+
+	<MODEL_TYPE extends IToolBarItemModel, BUILDER_TYPE extends IItemModelBuilder<?, MODEL_TYPE>> MODEL_TYPE addItem(
+		final BUILDER_TYPE itemBuilder);
+
+	<MODEL_TYPE extends IToolBarItemModel, BUILDER_TYPE extends IItemModelBuilder<?, MODEL_TYPE>> MODEL_TYPE addItem(
+		int index,
+		final BUILDER_TYPE itemBuilder);
+
+	void addAfter(IToolBarItemModel newItem, String id);
+
+	void addBefore(IToolBarItemModel newItem, String id);
+
+	IActionItemModel addAction(IAction action);
+
+	IActionItemModel addAction(final int index, IAction action);
+
+	IPopupActionItemModel addPopupAction(IAction action, IMenuModel popupMenu);
+
+	IPopupActionItemModel addPopupAction(final int index, IAction action, IMenuModel popupMenu);
+
+	IActionItemModel addActionItem();
+
+	IActionItemModel addActionItem(String text);
+
+	IActionItemModel addActionItem(String text, String toolTipText);
+
+	IActionItemModel addActionItem(String text, IImageConstant icon);
+
+	IActionItemModel addActionItem(String text, String toolTipText, IImageConstant icon);
+
+	IPopupActionItemModel addPopupActionItem();
+
+	IPopupActionItemModel addPopupActionItem(String text);
+
+	IPopupActionItemModel addPopupActionItem(String text, String toolTipText);
+
+	IPopupActionItemModel addPopupActionItem(String text, IImageConstant icon);
+
+	IPopupActionItemModel addPopupActionItem(String text, String toolTipText, IImageConstant icon);
+
+	ICheckedItemModel addCheckedItem();
+
+	ICheckedItemModel addCheckedItem(String text);
+
+	ICheckedItemModel addCheckedItem(String text, String toolTipText);
+
+	ICheckedItemModel addCheckedItem(String text, IImageConstant icon);
+
+	ICheckedItemModel addCheckedItem(String text, String toolTipText, IImageConstant icon);
+
+	IContainerItemModel addContainer();
+
+	IContainerItemModel addContainer(IContainerContentCreator contentCreator);
+
+	ISeparatorItemModel addSeparator();
+
+	ISeparatorItemModel addSeparator(String id);
+
+	ISeparatorItemModel addSeparator(int index);
+
+	void removeItem(final IToolBarItemModel item);
+
+	void removeItem(int index);
+
+	void removeAllItems();
+
+	void removeItemsOfModel(IToolBarModel toolBarModel);
+
+	IToolBarItemModel findItemById(String id);
+
+	List<IToolBarItemModel> getItems();
+
+	/**
+	 * Makes a deep copy of the tool bar and its children.
+	 * Registered listeners on items won't be copied.
+	 * 
+	 * @return A new instance that is a clone of this instance
+	 */
+	IToolBarModel createCopy();
+
+}
---
+++ tags/0.0.15/plugins/org.jowidgets.workbench.impl.rcp/src/org/jowidgets/workbench/impl/rcp/internal/WorkbenchContext.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2011, M. Woelker, H. Westphal
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of jo-widgets.org nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+package org.jowidgets.workbench.impl.rcp.internal;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
+import org.jowidgets.api.model.item.IMenuBarModel;
+import org.jowidgets.api.model.item.IToolBarModel;
+import org.jowidgets.api.widgets.IContainer;
+import org.jowidgets.common.application.IApplicationLifecycle;
+import org.jowidgets.util.Assert;
+import org.jowidgets.workbench.api.IComponent;
+import org.jowidgets.workbench.api.ITrayItem;
+import org.jowidgets.workbench.api.IWorkbench;
+import org.jowidgets.workbench.api.IWorkbenchApplication;
+import org.jowidgets.workbench.api.IWorkbenchContext;
+
+public final class WorkbenchContext implements IWorkbenchContext {
+
+	private final JoWorkbenchAdvisor workbenchAdvisor;
+	private IApplicationLifecycle lifecycle;
+	private IComponent currentComponent;
+
+	public WorkbenchContext(final IWorkbench workbench, final boolean saveAndRestore) {
+		workbenchAdvisor = new JoWorkbenchAdvisor(workbench, this, saveAndRestore);
+	}
+
+	public void setLifecycle(final IApplicationLifecycle lifecycle) {
+		this.lifecycle = lifecycle;
+	}
+
+	public void run() {
+		Assert.paramNotNull(lifecycle, "lifecycle");
+		try {
+			PlatformUI.createAndRunWorkbench(Display.getCurrent(), workbenchAdvisor);
+		}
+		finally {
+			lifecycle.finish();
+		}
+	}
+
+	@Override
+	public void add(final IWorkbenchApplication workbenchApplication) {
+		workbenchAdvisor.getWorkbenchWindowAdvisor().getApplicationFolder().addApplication(workbenchApplication);
+	}
+
+	@Override
+	public void add(final int index, final IWorkbenchApplication workbenchApplication) {
+		workbenchAdvisor.getWorkbenchWindowAdvisor().getApplicationFolder().addApplication(index, workbenchApplication);
+	}
+
+	@Override
+	public void remove(final IWorkbenchApplication workbenchApplication) {
+		workbenchAdvisor.getWorkbenchWindowAdvisor().getApplicationFolder().removeApplication(workbenchApplication);
+	}
+
+	@Override
+	public void finish() {
+		Assert.paramNotNull(lifecycle, "lifecycle");
+		try {
+			PlatformUI.getWorkbench().close();
+		}
+		finally {
+			lifecycle.finish();
+		}
+	}
+
+	@Override
+	public IContainer getStatusBar() {
+		return workbenchAdvisor.getWorkbenchWindowAdvisor().getStatusBar();
+	}
+
+	@Override
+	public ITrayItem getTrayItem() {
+		return workbenchAdvisor.getWorkbenchWindowAdvisor().getTrayItem();
+	}
+
+	@Override
+	public void addShutdownHook(final Runnable shutdownHook) {
+		workbenchAdvisor.addShutdownHook(shutdownHook);
+	}
+
+	@Override
+	public void removeShutdownHook(final Runnable shutdownHook) {
+		workbenchAdvisor.removeShutdownHook(shutdownHook);
+	}
+
+	public void setFolderRatio(final double folderRatio) {
+		workbenchAdvisor.setFolderRatio(folderRatio);
+	}
+
+	public double getFolderRatio() {
+		return workbenchAdvisor.getWorkbenchWindowAdvisor().getFolderRatio();
+	}
+
+	public void setSelectedTreeNode(final String[] selectedTreeNode) {
+		workbenchAdvisor.setSelectedTreeNode(selectedTreeNode);
+	}
+
+	public String[] getSelectedTreeNode() {
+		return workbenchAdvisor.getWorkbenchWindowAdvisor().getSelectedTreeNode();
+	}
+
+	@Override
+	public IToolBarModel getToolBar() {
+		return workbenchAdvisor.getWorkbenchWindowAdvisor().getToolBar();
+	}
+
+	@Override
+	public IMenuBarModel getMenuBar() {
+		return workbenchAdvisor.getWorkbenchWindowAdvisor().getMenuBar();
+	}
+
+	public IComponent getCurrentComponent() {
+		return currentComponent;
+	}
+
+	public void setCurrentComponent(final IComponent currentComponent) {
+		this.currentComponent = currentComponent;
+	}
+
+	public void selectTreeNode(final String[] selectedTreeNode) {
+		workbenchAdvisor.getWorkbenchWindowAdvisor().selectTreeNode(selectedTreeNode);
+	}
+
+}
