bf53439c9e54f8c5b9498f2135f2954fdee2f8d0
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2011-03-04 12:37:42 +0000

    [maven-scm] copy for tag org.jowidgets.parent-0.0.16

---
+++ tags/org.jowidgets.parent-0.0.16/core/org.jowidgets.spi.impl.swt/src/main/java/org/jowidgets/spi/impl/swt/widgets/MenuBarImpl.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2010, grossmann
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+package org.jowidgets.spi.impl.swt.widgets;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.jowidgets.spi.widgets.IMainMenuSpi;
+import org.jowidgets.spi.widgets.IMenuBarSpi;
+
+public class MenuBarImpl extends SwtMenu implements IMenuBarSpi {
+
+	public MenuBarImpl(final Menu menu) {
+		super(menu);
+	}
+
+	@Override
+	public IMainMenuSpi addMenu(final Integer index) {
+		MenuItem menuItem = null;
+		if (index != null) {
+			menuItem = new MenuItem(getUiReference(), SWT.CASCADE, index.intValue());
+		}
+		else {
+			menuItem = new MenuItem(getUiReference(), SWT.CASCADE);
+		}
+
+		final Menu menu = new Menu(getUiReference().getShell(), SWT.DROP_DOWN);
+		menuItem.setMenu(menu);
+		return new MainMenuImpl(menuItem, menu);
+	}
+
+}
