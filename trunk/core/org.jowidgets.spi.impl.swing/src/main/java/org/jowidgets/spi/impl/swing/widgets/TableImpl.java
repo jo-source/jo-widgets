8fa8f22eb5e7cb1447f7fe06271126aaf890df06
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2011-03-12 16:42:00 +0000

    started table implementation for swing

---
+++ trunk/core/org.jowidgets.spi.impl.swing/src/main/java/org/jowidgets/spi/impl/swing/widgets/TableImpl.java
@@ -0,0 +1,439 @@
+/*
+ * Copyright (c) 2011, grossmann
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+package org.jowidgets.spi.impl.swing.widgets;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+
+import org.jowidgets.common.color.IColorConstant;
+import org.jowidgets.common.image.IImageConstant;
+import org.jowidgets.common.types.AlignmentHorizontal;
+import org.jowidgets.common.types.Dimension;
+import org.jowidgets.common.types.Markup;
+import org.jowidgets.common.types.Position;
+import org.jowidgets.common.types.SelectionPolicy;
+import org.jowidgets.common.types.TableColumnPackPolicy;
+import org.jowidgets.common.widgets.controler.ITableCellEditorListener;
+import org.jowidgets.common.widgets.controler.ITableCellListener;
+import org.jowidgets.common.widgets.controler.ITableCellPopupDetectionListener;
+import org.jowidgets.common.widgets.controler.ITableColumnListener;
+import org.jowidgets.common.widgets.controler.ITableColumnPopupDetectionListener;
+import org.jowidgets.common.widgets.controler.ITableSelectionListener;
+import org.jowidgets.common.widgets.model.ITableCell;
+import org.jowidgets.common.widgets.model.ITableColumn;
+import org.jowidgets.common.widgets.model.ITableColumnModel;
+import org.jowidgets.common.widgets.model.ITableColumnModelListener;
+import org.jowidgets.common.widgets.model.ITableModel;
+import org.jowidgets.common.widgets.model.ITableModelListener;
+import org.jowidgets.spi.impl.controler.TableCellEditorObservable;
+import org.jowidgets.spi.impl.controler.TableCellObservable;
+import org.jowidgets.spi.impl.controler.TableCellPopupDetectionObservable;
+import org.jowidgets.spi.impl.controler.TableColumnObservable;
+import org.jowidgets.spi.impl.controler.TableColumnPopupDetectionObservable;
+import org.jowidgets.spi.impl.controler.TableSelectionObservable;
+import org.jowidgets.spi.impl.swing.image.SwingImageRegistry;
+import org.jowidgets.spi.impl.swing.util.AlignmentConvert;
+import org.jowidgets.spi.impl.swing.util.ColorConvert;
+import org.jowidgets.spi.impl.swing.util.FontProvider;
+import org.jowidgets.spi.widgets.ITableSpi;
+import org.jowidgets.spi.widgets.setup.ITableSetupSpi;
+
+public class TableImpl extends SwingControl implements ITableSpi {
+
+	private final JTable table;
+	private final ITableModel tableModel;
+	private final ITableColumnModel columnModel;
+	private final CellRenderer cellRenderer;
+
+	private final TableCellObservable tableCellObservable;
+	private final TableCellPopupDetectionObservable tableCellPopupDetectionObservable;
+	private final TableColumnPopupDetectionObservable tableColumnPopupDetectionObservable;
+	private final TableColumnObservable tableColumnObservable;
+	private final TableSelectionObservable tableSelectionObservable;
+	private final TableCellEditorObservable tableCellEditorObservable;
+
+	private final boolean columnsResizeable;
+
+	public TableImpl(final ITableSetupSpi setup) {
+		super(new JScrollPane(new JTable()));
+
+		this.tableCellObservable = new TableCellObservable();
+		this.tableCellPopupDetectionObservable = new TableCellPopupDetectionObservable();
+		this.tableColumnPopupDetectionObservable = new TableColumnPopupDetectionObservable();
+		this.tableColumnObservable = new TableColumnObservable();
+		this.tableSelectionObservable = new TableSelectionObservable();
+		this.tableCellEditorObservable = new TableCellEditorObservable();
+
+		this.tableModel = setup.getTableModel();
+		this.columnModel = setup.getColumnModel();
+
+		this.columnsResizeable = setup.getColumnsResizeable();
+
+		this.table = (JTable) getUiReference().getViewport().getView();
+
+		table.setGridColor(new Color(230, 230, 230));
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		table.getTableHeader().setReorderingAllowed(setup.getColumnsMoveable());
+
+		if (setup.getSelectionPolicy() == SelectionPolicy.SINGLE_SELECTION) {
+			table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		}
+		else {
+			table.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		}
+
+		table.getTableHeader().setDefaultRenderer(new TableHeaderRenderer());
+
+		table.setModel(new SwingTableModel(setup.getTableModel(), setup.getColumnModel()));
+
+		this.cellRenderer = new CellRenderer();
+
+		final Enumeration<TableColumn> columns = table.getColumnModel().getColumns();
+		while (columns.hasMoreElements()) {
+			final TableColumn column = columns.nextElement();
+			column.setCellRenderer(cellRenderer);
+			column.setResizable(columnsResizeable);
+		}
+	}
+
+	@Override
+	public JScrollPane getUiReference() {
+		return (JScrollPane) super.getUiReference();
+	}
+
+	@Override
+	public void initialize() {
+
+	}
+
+	@Override
+	public Position getCellPosition(final int rowIndex, final int columnIndex) {
+		return null;
+	}
+
+	@Override
+	public Dimension getCellSize(final int rowIndex, final int columnIndex) {
+		return null;
+	}
+
+	@Override
+	public void pack(final TableColumnPackPolicy policy) {
+
+	}
+
+	@Override
+	public void pack(final int columnIndex, final TableColumnPackPolicy policy) {
+
+	}
+
+	@Override
+	public ArrayList<Integer> getColumnPermutation() {
+		final ArrayList<Integer> result = new ArrayList<Integer>();
+		//TODO MG implement getColumnPermutation
+		return result;
+	}
+
+	@Override
+	public ArrayList<Integer> getSelection() {
+		final ArrayList<Integer> result = new ArrayList<Integer>();
+		//TODO MG implement getSelection
+		return result;
+	}
+
+	@Override
+	public void setSelection(final ArrayList<Integer> selection) {
+
+	}
+
+	@Override
+	public void addTableCellListener(final ITableCellListener listener) {
+		tableCellObservable.addTableCellListener(listener);
+	}
+
+	@Override
+	public void removeTableCellListener(final ITableCellListener listener) {
+		tableCellObservable.removeTableCellListener(listener);
+	}
+
+	@Override
+	public void addTableCellPopupDetectionListener(final ITableCellPopupDetectionListener listener) {
+		tableCellPopupDetectionObservable.addTableCellPopupDetectionListener(listener);
+	}
+
+	@Override
+	public void removeTableCellPopupDetectionListener(final ITableCellPopupDetectionListener listener) {
+		tableCellPopupDetectionObservable.removeTableCellPopupDetectionListener(listener);
+	}
+
+	@Override
+	public void addTableColumnPopupDetectionListener(final ITableColumnPopupDetectionListener listener) {
+		tableColumnPopupDetectionObservable.addTableColumnPopupDetectionListener(listener);
+	}
+
+	@Override
+	public void removeTableColumnPopupDetectionListener(final ITableColumnPopupDetectionListener listener) {
+		tableColumnPopupDetectionObservable.addTableColumnPopupDetectionListener(listener);
+	}
+
+	@Override
+	public void addTableColumnListener(final ITableColumnListener listener) {
+		tableColumnObservable.addTableColumnListener(listener);
+	}
+
+	@Override
+	public void removeTableColumnListener(final ITableColumnListener listener) {
+		tableColumnObservable.removeTableColumnListener(listener);
+	}
+
+	@Override
+	public void addTableSelectionListener(final ITableSelectionListener listener) {
+		tableSelectionObservable.addTableSelectionListener(listener);
+	}
+
+	@Override
+	public void removeTableSelectionListener(final ITableSelectionListener listener) {
+		tableSelectionObservable.removeTableSelectionListener(listener);
+	}
+
+	@Override
+	public void addTableCellEditorListener(final ITableCellEditorListener listener) {
+		tableCellEditorObservable.addTableCellEditorListener(listener);
+	}
+
+	@Override
+	public void removeTableCellEditorListener(final ITableCellEditorListener listener) {
+		tableCellEditorObservable.removeTableCellEditorListener(listener);
+	}
+
+	final class TableModelListener implements ITableModelListener {
+
+		@Override
+		public void rowsAdded(final int[] rowIndices) {
+			// TODO MG better implementation of rowsAdded observe
+			rowsStructureChanged();
+		}
+
+		@Override
+		public void rowsRemoved(final int[] rowIndices) {
+			// TODO MG better implementation of rowsRemoved observe
+			rowsStructureChanged();
+		}
+
+		@Override
+		public void rowsChanged(final int[] rowIndices) {
+			// TODO MG implement rowsChanged observe
+		}
+
+		@Override
+		public void rowsStructureChanged() {
+			// TODO MG implement rowsChanged observe
+		}
+
+		@Override
+		public void selectionChanged() {
+			setSelection(tableModel.getSelection());
+		}
+
+	}
+
+	final class TableColumnsModelListener implements ITableColumnModelListener {
+
+		@Override
+		public void columnsAdded(final int[] columnIndices) {
+			// TODO MG better implementation of columnsAdded observe
+			columnsStructureChanged();
+		}
+
+		@Override
+		public void columnsRemoved(final int[] columnIndices) {
+			// TODO MG better implementation of columnsRemoved observe
+			columnsStructureChanged();
+		}
+
+		@Override
+		public void columnsChanged(final int[] columnIndices) {
+			// TODO MG implement columnsChanged observe
+		}
+
+		@Override
+		public void columnsStructureChanged() {
+			// TODO MG implement columnsStructureChanged observe
+		}
+
+	}
+
+	class SwingTableModel extends AbstractTableModel {
+
+		private static final long serialVersionUID = 6096723765272552285L;
+
+		private final ITableModel tableModel;
+		private final ITableColumnModel columnModel;
+
+		public SwingTableModel(final ITableModel tableModel, final ITableColumnModel columnModel) {
+			super();
+			this.tableModel = tableModel;
+			this.columnModel = columnModel;
+		}
+
+		@Override
+		public String getColumnName(final int column) {
+			return columnModel.getColumn(column).getText();
+		}
+
+		@Override
+		public Object getValueAt(final int rowIndex, final int columnIndex) {
+			return tableModel.getCell(rowIndex, columnIndex);
+		}
+
+		@Override
+		public int getRowCount() {
+			return tableModel.getRowCount();
+		}
+
+		@Override
+		public int getColumnCount() {
+			return columnModel.getColumnCount();
+		}
+
+	}
+
+	class TableHeaderRenderer implements TableCellRenderer {
+
+		private final TableCellRenderer headerRenderer = table.getTableHeader().getDefaultRenderer();
+
+		@Override
+		public Component getTableCellRendererComponent(
+			final JTable table,
+			final Object value,
+			final boolean isSelected,
+			final boolean hasFocus,
+			final int row,
+			final int columnIndex) {
+			final JLabel defaultComponent = (JLabel) headerRenderer.getTableCellRendererComponent(
+					table,
+					value,
+					isSelected,
+					hasFocus,
+					row,
+					columnIndex);
+
+			final ITableColumn column = columnModel.getColumn(columnIndex);
+
+			if (column.getIcon() != null) {
+				defaultComponent.setIcon(SwingImageRegistry.getInstance().getImageIcon(column.getIcon()));
+			}
+			defaultComponent.setToolTipText(column.getToolTipText());
+
+			return defaultComponent;
+		}
+
+	};
+
+	class CellRenderer extends DefaultTableCellRenderer {
+
+		private static final long serialVersionUID = 1581242312833196056L;
+
+		@Override
+		public Component getTableCellRendererComponent(
+			final JTable table,
+			final Object value,
+			final boolean isSelected,
+			final boolean hasFocus,
+			final int row,
+			final int column) {
+
+			final ITableCell cell = (ITableCell) value;
+
+			final Component result = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+			if (cell != null) {
+				final IImageConstant icon = cell.getIcon();
+				final IColorConstant backgroundColor = cell.getBackgroundColor();
+				final IColorConstant foregroundColor = cell.getForegroundColor();
+				final Markup markup = cell.getMarkup();
+				final AlignmentHorizontal alignment = columnModel.getColumn(column).getAlignment();
+
+				setText(cell.getText());
+				setToolTipText(cell.getToolTipText());
+
+				if (icon != null) {
+					setIcon(SwingImageRegistry.getInstance().getImageIcon(icon));
+				}
+				else {
+					setIcon(null);
+				}
+
+				if (markup != null) {
+					setFont(FontProvider.deriveFont(getFont(), markup));
+				}
+				else {
+					setFont(FontProvider.deriveFont(getFont(), Markup.DEFAULT));
+				}
+
+				if (alignment != null) {
+					setHorizontalAlignment(AlignmentConvert.convert(alignment));
+				}
+				else {
+					setHorizontalAlignment(JLabel.LEFT);
+				}
+
+				if (!isSelected) {
+					if (backgroundColor != null) {
+						setBackground(ColorConvert.convert(backgroundColor));
+					}
+					else {
+						setBackground(null);
+					}
+
+					if (foregroundColor != null) {
+						setForeground(ColorConvert.convert(foregroundColor));
+					}
+					else {
+						setForeground(null);
+					}
+				}
+
+			}
+
+			return result;
+		}
+	}
+
+}
