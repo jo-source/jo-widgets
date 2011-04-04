8f4a0cbf3602331c338d8ec279e2829e9e63dea7
herr.grossmann <herrgrossmann@users.noreply.github.com>
2011-04-04 19:41:15 +0000

    new interface ITextInputVerifier added

--- core/org.jowidgets.api/src/main/java/org/jowidgets/api/validation/ITextInputVerifier.java
+++ core/org.jowidgets.api/src/main/java/org/jowidgets/api/validation/ITextInputVerifier.java
@@ -1,22 +1,22 @@
 /*
- * Copyright (c) 2010, Michael Grossmann
+ * Copyright (c) 2011, grossmann
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
+ *   notice, this list of conditions and the following disclaimer.
  * * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
  * * Neither the name of the jo-widgets.org nor the
- * names of its contributors may be used to endorse or promote products
- * derived from this software without specific prior written permission.
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
@@ -25,8 +25,21 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
  * DAMAGE.
  */
+
 package org.jowidgets.api.validation;
 
-public interface ITextInputValidator extends ITextInputVerifier, IValidator<String> {
+public interface ITextInputVerifier {
+
+	/**
+	 * Checks a string if it could be completed to a valid input.
+	 * 
+	 * @param string
+	 *            the string to check
+	 * 
+	 * @return OK if the string is valid or if the string could be completed to
+	 *         a valid input by adding substring at any position(s), ERROR with
+	 *         valuable description otherwise
+	 */
+	ValidationMessage isCompletableToValid(String string);
 
 }
