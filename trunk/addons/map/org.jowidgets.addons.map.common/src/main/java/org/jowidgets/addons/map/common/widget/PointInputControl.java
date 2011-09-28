719857da21b7a24108ebe528ef0a36601b2aeb48
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2011-09-28 15:07:55 +0000

    use wrapper

--- trunk/addons/map/org.jowidgets.addons.map.common/src/main/java/org/jowidgets/addons/map/common/widget/PointInputControl.java
+++ trunk/addons/map/org.jowidgets.addons.map.common/src/main/java/org/jowidgets/addons/map/common/widget/PointInputControl.java
@@ -31,28 +31,15 @@ package org.jowidgets.addons.map.common.widget;
 import org.jowidgets.addons.map.common.IAvailableCallback;
 import org.jowidgets.addons.map.common.IDesignationListener;
 import org.jowidgets.addons.map.common.IMapContext;
-import org.jowidgets.api.model.item.IMenuModel;
 import org.jowidgets.api.toolkit.Toolkit;
-import org.jowidgets.api.widgets.IContainer;
-import org.jowidgets.api.widgets.IPopupMenu;
-import org.jowidgets.api.widgets.blueprint.factory.IBluePrintFactory;
-import org.jowidgets.common.color.IColorConstant;
-import org.jowidgets.common.types.Cursor;
-import org.jowidgets.common.types.Dimension;
-import org.jowidgets.common.types.Position;
-import org.jowidgets.common.widgets.IComponentCommon;
-import org.jowidgets.common.widgets.controller.IComponentListener;
-import org.jowidgets.common.widgets.controller.IFocusListener;
 import org.jowidgets.common.widgets.controller.IInputListener;
-import org.jowidgets.common.widgets.controller.IKeyListener;
-import org.jowidgets.common.widgets.controller.IMouseListener;
-import org.jowidgets.common.widgets.controller.IPopupDetectionListener;
 import org.jowidgets.impl.widgets.basic.factory.internal.util.ColorSettingsInvoker;
 import org.jowidgets.impl.widgets.basic.factory.internal.util.VisibiliySettingsInvoker;
 import org.jowidgets.tools.controller.InputObservable;
 import org.jowidgets.tools.validation.CompoundValidator;
 import org.jowidgets.tools.validation.ValidationCache;
 import org.jowidgets.tools.validation.ValidationCache.IValidationResultCreator;
+import org.jowidgets.tools.widgets.wrapper.ControlWrapper;
 import org.jowidgets.util.NullCompatibleEquivalence;
 import org.jowidgets.validation.IValidationConditionListener;
 import org.jowidgets.validation.IValidationResult;
@@ -62,7 +49,7 @@ import de.micromata.opengis.kml.v_2_2_0.KmlFactory;
 import de.micromata.opengis.kml.v_2_2_0.Placemark;
 import de.micromata.opengis.kml.v_2_2_0.Point;
 
-final class PointInputControl implements IPointInputControl {
+final class PointInputControl extends ControlWrapper implements IPointInputControl {
 
 	private static final String PLACEMARK_ID = PointInputControl.class.getSimpleName();
 
@@ -77,8 +64,11 @@ final class PointInputControl implements IPointInputControl {
 	private boolean editable;
 
 	PointInputControl(final Object parentUiReference, final IPointInputControlBlueprint descriptor) {
-		final IBluePrintFactory bpF = Toolkit.getBluePrintFactory();
-		mapWidget = Toolkit.getWidgetFactory().create(parentUiReference, bpF.bluePrint(IMapWidgetBlueprint.class));
+		super(Toolkit.getWidgetFactory().create(
+				parentUiReference,
+				Toolkit.getBluePrintFactory().bluePrint(IMapWidgetBlueprint.class)));
+		this.mapWidget = (IMapWidget) getWidget();
+
 		VisibiliySettingsInvoker.setVisibility(descriptor, this);
 		ColorSettingsInvoker.setColors(descriptor, this);
 
@@ -200,241 +190,6 @@ final class PointInputControl implements IPointInputControl {
 	@Override
 	public void removeInputListener(final IInputListener listener) {
 		inputObservable.removeInputListener(listener);
-	}
-
-	@Override
-	public void addKeyListener(final IKeyListener listener) {
-		mapWidget.addKeyListener(listener);
-	}
-
-	@Override
-	public void addFocusListener(final IFocusListener listener) {
-		mapWidget.addFocusListener(listener);
-	}
-
-	@Override
-	public void addMouseListener(final IMouseListener mouseListener) {
-		mapWidget.addMouseListener(mouseListener);
-	}
-
-	@Override
-	public void addComponentListener(final IComponentListener componentListener) {
-		mapWidget.addComponentListener(componentListener);
-	}
-
-	@Override
-	public Object getUiReference() {
-		return mapWidget.getUiReference();
-	}
-
-	@Override
-	public void addPopupDetectionListener(final IPopupDetectionListener listener) {
-		mapWidget.addPopupDetectionListener(listener);
-	}
-
-	@Override
-	public void removeKeyListener(final IKeyListener listener) {
-		mapWidget.removeKeyListener(listener);
-	}
-
-	@Override
-	public void removeFocusListener(final IFocusListener listener) {
-		mapWidget.removeFocusListener(listener);
-	}
-
-	@Override
-	public void removeMouseListener(final IMouseListener mouseListener) {
-		mapWidget.removeMouseListener(mouseListener);
-	}
-
-	@Override
-	public void setToolTipText(final String toolTip) {
-		mapWidget.setToolTipText(toolTip);
-	}
-
-	@Override
-	public void removeComponentListener(final IComponentListener componentListener) {
-		mapWidget.removeComponentListener(componentListener);
-	}
-
-	@Override
-	public void removePopupDetectionListener(final IPopupDetectionListener listener) {
-		mapWidget.removePopupDetectionListener(listener);
-	}
-
-	@Override
-	public void setLayoutConstraints(final Object layoutConstraints) {
-		mapWidget.setLayoutConstraints(layoutConstraints);
-	}
-
-	@Override
-	public void setParent(final IContainer parent) {
-		mapWidget.setParent(parent);
-	}
-
-	@Override
-	public IContainer getParent() {
-		return mapWidget.getParent();
-	}
-
-	@Override
-	public Object getLayoutConstraints() {
-		return mapWidget.getLayoutConstraints();
-	}
-
-	@Override
-	public boolean isReparentable() {
-		return mapWidget.isReparentable();
-	}
-
-	@Override
-	public Dimension getMinSize() {
-		return mapWidget.getMinSize();
-	}
-
-	@Override
-	public void setMinSize(final Dimension minSize) {
-		mapWidget.setMinSize(minSize);
-	}
-
-	@Override
-	public IPopupMenu createPopupMenu() {
-		return mapWidget.createPopupMenu();
-	}
-
-	@Override
-	public Dimension getPreferredSize() {
-		return mapWidget.getPreferredSize();
-	}
-
-	@Override
-	public void setPopupMenu(final IMenuModel popupMenu) {
-		mapWidget.setPopupMenu(popupMenu);
-	}
-
-	@Override
-	public void setEnabled(final boolean enabled) {
-		mapWidget.setEnabled(enabled);
-	}
-
-	@Override
-	public void setPreferredSize(final Dimension preferredSize) {
-		mapWidget.setPreferredSize(preferredSize);
-	}
-
-	@Override
-	public Dimension getMaxSize() {
-		return mapWidget.getMaxSize();
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return mapWidget.isEnabled();
-	}
-
-	@Override
-	public void setMaxSize(final Dimension maxSize) {
-		mapWidget.setMaxSize(maxSize);
-	}
-
-	@Override
-	public Position toScreen(final Position localPosition) {
-		return mapWidget.toScreen(localPosition);
-	}
-
-	@Override
-	public Position toLocal(final Position screenPosition) {
-		return mapWidget.toLocal(screenPosition);
-	}
-
-	@Override
-	public void setSize(final int width, final int height) {
-		mapWidget.setSize(width, height);
-	}
-
-	@Override
-	public void redraw() {
-		mapWidget.redraw();
-	}
-
-	@Override
-	public void setPosition(final int x, final int y) {
-		mapWidget.setPosition(x, y);
-	}
-
-	@Override
-	public Position fromComponent(final IComponentCommon component, final Position componentPosition) {
-		return mapWidget.fromComponent(component, componentPosition);
-	}
-
-	@Override
-	public void setRedrawEnabled(final boolean enabled) {
-		mapWidget.setRedrawEnabled(enabled);
-	}
-
-	@Override
-	public Position toComponent(final Position componentPosition, final IComponentCommon component) {
-		return mapWidget.toComponent(componentPosition, component);
-	}
-
-	@Override
-	public boolean requestFocus() {
-		return mapWidget.requestFocus();
-	}
-
-	@Override
-	public void setForegroundColor(final IColorConstant colorValue) {
-		mapWidget.setForegroundColor(colorValue);
-	}
-
-	@Override
-	public void setBackgroundColor(final IColorConstant colorValue) {
-		mapWidget.setBackgroundColor(colorValue);
-	}
-
-	@Override
-	public IColorConstant getForegroundColor() {
-		return mapWidget.getForegroundColor();
-	}
-
-	@Override
-	public IColorConstant getBackgroundColor() {
-		return mapWidget.getBackgroundColor();
-	}
-
-	@Override
-	public void setCursor(final Cursor cursor) {
-		mapWidget.setCursor(cursor);
-	}
-
-	@Override
-	public void setVisible(final boolean visible) {
-		mapWidget.setVisible(visible);
-	}
-
-	@Override
-	public boolean isVisible() {
-		return mapWidget.isVisible();
-	}
-
-	@Override
-	public Dimension getSize() {
-		return mapWidget.getSize();
-	}
-
-	@Override
-	public void setSize(final Dimension size) {
-		mapWidget.setSize(size);
-	}
-
-	@Override
-	public Position getPosition() {
-		return mapWidget.getPosition();
-	}
-
-	@Override
-	public void setPosition(final Position position) {
-		mapWidget.setPosition(position);
 	}
 
 }
