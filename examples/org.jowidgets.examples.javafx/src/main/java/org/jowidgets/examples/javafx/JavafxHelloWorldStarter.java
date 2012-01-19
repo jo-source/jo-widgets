68023ef80c5a718763d6a5f5abfba8c71a8ae36c
herr.grossmann <herrgrossmann@users.noreply.github.com>
2012-01-19 17:25:42 +0000

    renamed package

--- examples/org.jowidgets.examples.javafx/src/main/java/org/jowidgets/examples/javafx/JavafxHelloWorldStarter.java
+++ examples/org.jowidgets.examples.javafx/src/main/java/org/jowidgets/examples/javafx/JavafxHelloWorldStarter.java
@@ -25,7 +25,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
  * DAMAGE.
  */
-package org.jowidgets.examples.swing;
+package org.jowidgets.examples.javafx;
 
 import org.jowidgets.examples.common.HelloWorldApplication;
 
