464b5422d99449e104ac39361ec138f4cea41e90
herr.grossmann <herrgrossmann@users.noreply.github.com>
2012-02-24 15:24:58 +0000

    added swt spi common

---
+++ core/org.jowidgets.spi.impl.swt.common/src/main/java/org/jowidgets/spi/impl/swt/common/widgets/CalendarImpl.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2010, Michael Grossmann
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+package org.jowidgets.spi.impl.swt.common.widgets;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DateTime;
+import org.jowidgets.common.widgets.controller.IInputListener;
+import org.jowidgets.spi.impl.controller.InputObservable;
+import org.jowidgets.spi.widgets.ICalendarSpi;
+import org.jowidgets.spi.widgets.setup.ICalendarSetupSpi;
+import org.jowidgets.util.Assert;
+
+public class CalendarImpl extends SwtControl implements ICalendarSpi {
+
+	private final InputObservable inputObservable;
+
+	private Date date;
+
+	public CalendarImpl(final Object parentUiReference, final ICalendarSetupSpi setup) {
+		super(new DateTime((Composite) parentUiReference, SWT.CALENDAR));
+
+		this.inputObservable = new InputObservable();
+
+		getUiReference().addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(final SelectionEvent e) {
+				final Calendar calendar = new GregorianCalendar();
+				calendar.set(Calendar.YEAR, getUiReference().getYear());
+				calendar.set(Calendar.MONTH, getUiReference().getMonth() + 1);
+				calendar.set(Calendar.DAY_OF_MONTH, getUiReference().getDay());
+				calendar.set(Calendar.HOUR_OF_DAY, 0);
+				calendar.set(Calendar.MINUTE, 0);
+				calendar.set(Calendar.SECOND, 0);
+				calendar.set(Calendar.MILLISECOND, 0);
+				date = calendar.getTime();
+				inputObservable.fireInputChanged(date);
+			}
+
+		});
+	}
+
+	@Override
+	public DateTime getUiReference() {
+		return (DateTime) super.getUiReference();
+	}
+
+	@Override
+	public void setDate(final Date date) {
+		Assert.paramNotNull(date, "date");
+		final Calendar calendar = new GregorianCalendar();
+		calendar.setTime(date);
+		getUiReference().setDate(
+				calendar.get(Calendar.YEAR),
+				calendar.get(Calendar.MONTH) - 1,
+				calendar.get(Calendar.DAY_OF_MONTH));
+	}
+
+	@Override
+	public Date getDate() {
+		return date;
+	}
+
+	@Override
+	public void addInputListener(final IInputListener listener) {
+		inputObservable.addInputListener(listener);
+	}
+
+	@Override
+	public void removeInputListener(final IInputListener listener) {
+		inputObservable.removeInputListener(listener);
+	}
+
+}
---
+++ core/org.jowidgets.spi.impl.swt.common/src/main/java/org/jowidgets/spi/impl/swt/common/widgets/CheckBoxImpl.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2010, Michael Grossmann
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+package org.jowidgets.spi.impl.swt.common.widgets;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.jowidgets.common.types.Markup;
+import org.jowidgets.spi.impl.swt.common.util.AlignmentConvert;
+import org.jowidgets.spi.impl.swt.common.util.FontProvider;
+import org.jowidgets.spi.widgets.ICheckBoxSpi;
+import org.jowidgets.spi.widgets.setup.ICheckBoxSetupSpi;
+
+public class CheckBoxImpl extends AbstractInputControl implements ICheckBoxSpi {
+
+	public CheckBoxImpl(final Object parentUiReference, final ICheckBoxSetupSpi setup) {
+		this(new Button((Composite) parentUiReference, SWT.NONE | SWT.CHECK), setup);
+	}
+
+	public CheckBoxImpl(final Button button, final ICheckBoxSetupSpi setup) {
+		super(button);
+
+		setText(setup.getText());
+		setToolTipText(setup.getToolTipText());
+
+		setMarkup(setup.getMarkup());
+
+		getUiReference().setAlignment(AlignmentConvert.convert(setup.getAlignment()));
+
+		getUiReference().addListener(SWT.Selection, new Listener() {
+			@Override
+			public void handleEvent(final Event event) {
+				fireInputChanged(getUiReference().getSelection());
+			}
+		});
+	}
+
+	@Override
+	public Button getUiReference() {
+		return (Button) super.getUiReference();
+	}
+
+	@Override
+	public void setEditable(final boolean editable) {
+		getUiReference().setEnabled(editable);
+	}
+
+	@Override
+	public void setMarkup(final Markup markup) {
+		final Button button = this.getUiReference();
+		final Font newFont = FontProvider.deriveFont(button.getFont(), markup);
+		button.setFont(newFont);
+	}
+
+	@Override
+	public void setFontSize(final int size) {
+		getUiReference().setFont(FontProvider.deriveFont(getUiReference().getFont(), size));
+	}
+
+	@Override
+	public void setFontName(final String fontName) {
+		getUiReference().setFont(FontProvider.deriveFont(getUiReference().getFont(), fontName));
+	}
+
+	@Override
+	public void setText(final String text) {
+		if (text != null) {
+			getUiReference().setText(text);
+		}
+		else {
+			setText("");
+		}
+	}
+
+	@Override
+	public boolean isSelected() {
+		return getUiReference().getSelection();
+	}
+
+	@Override
+	public void setSelected(final boolean selected) {
+		getUiReference().setSelection(selected);
+		fireInputChanged(selected);
+	}
+
+}
