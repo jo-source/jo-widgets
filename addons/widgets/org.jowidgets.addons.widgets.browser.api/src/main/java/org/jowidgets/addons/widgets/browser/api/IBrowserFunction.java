bdb8bc0d1155bd03c82931fd8369a05db36c1a23
herr.grossmann <herrgrossmann@users.noreply.github.com>
2012-03-04 08:37:19 +0000

    added IBrowserFunction support to browser

--- addons/widgets/org.jowidgets.addons.widgets.browser.api/src/main/java/org/jowidgets/addons/widgets/browser/api/IBrowserFunction.java
+++ addons/widgets/org.jowidgets.addons.widgets.browser.api/src/main/java/org/jowidgets/addons/widgets/browser/api/IBrowserFunction.java
@@ -28,26 +28,23 @@
 
 package org.jowidgets.addons.widgets.browser.api;
 
-import org.jowidgets.api.widgets.IControl;
+public interface IBrowserFunction {
 
-public interface IBrowser extends IControl {
-
-	void setUrl(String url);
-
-	void setHtml(String html);
-
-	Object evaluateScript(String javaScript);
-
-	boolean executeScript(String javaScript);
-
-	IBrowserFunctionHandle createBrowserFunction(String functionName, IBrowserFunction function);
-
-	void addLocationListener(IBrowserLocationListener listener);
-
-	void removeLocationListener(IBrowserLocationListener listener);
-
-	void addProgressListener(IBrowserProgressListener listener);
-
-	void removeProgressListener(IBrowserProgressListener listener);
+	/**
+	 * Will be invoked if java script function with the browser function name will be invoked.
+	 * 
+	 * Supported argument and return types are:
+	 * 
+	 * javascript type null or undefined -> <code>null</code> <br>
+	 * javascript type number -> <code>java.lang.Double</code> <br>
+	 * javascript type string -> <code>java.lang.String</code> <br>
+	 * javascript type boolean -> <code>java.lang.Boolean</code> <br>
+	 * javascript array (all types must be supported) -> <code>java.lang.Object[]</code> (converted as shown above)
+	 * 
+	 * @param args The functions arguments
+	 * 
+	 * @return The result of the invocation
+	 */
+	Object invoke(Object[] args);
 
 }
