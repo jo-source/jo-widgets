b7582ef6b4a8cf988683501a7a8051b8148142c0
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2013-03-03 21:02:02 +0000

    added workbench demo3

---
+++ trunk/examples/org.jowidgets.examples.common/src/main/java/org/jowidgets/examples/common/workbench/demo3/command/CreatePersonActionFactory.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2013, grossmann
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+package org.jowidgets.examples.common.workbench.demo3.command;
+
+import org.jowidgets.addons.icons.silkicons.SilkIcons;
+import org.jowidgets.api.command.CommandAction;
+import org.jowidgets.api.command.IAction;
+import org.jowidgets.api.command.IActionBuilder;
+import org.jowidgets.api.command.ICommandExecutor;
+import org.jowidgets.api.command.IExecutionContext;
+import org.jowidgets.api.toolkit.Toolkit;
+import org.jowidgets.api.widgets.IInputDialog;
+import org.jowidgets.api.widgets.blueprint.IInputDialogBluePrint;
+import org.jowidgets.common.types.Dimension;
+import org.jowidgets.examples.common.workbench.demo3.form.PersonContentCreator;
+import org.jowidgets.examples.common.workbench.demo3.model.BeanTableModel;
+import org.jowidgets.examples.common.workbench.demo3.model.Person;
+import org.jowidgets.tools.widgets.blueprint.BPF;
+
+public final class CreatePersonActionFactory {
+
+	private CreatePersonActionFactory() {}
+
+	public static IAction create(final BeanTableModel<Person> model) {
+		final IActionBuilder builder = CommandAction.builder();
+		builder.setText("Create person ...");
+		builder.setIcon(SilkIcons.USER_ADD);
+		builder.setCommand(new CreatePersonCommand(model));
+		return builder.build();
+	}
+
+	private static final class CreatePersonCommand implements ICommandExecutor {
+
+		private final BeanTableModel<Person> model;
+
+		private CreatePersonCommand(final BeanTableModel<Person> model) {
+			this.model = model;
+		}
+
+		@Override
+		public void execute(final IExecutionContext executionContext) throws Exception {
+			final IInputDialogBluePrint<Person> dialogBp = BPF.inputDialog(new PersonContentCreator(false));
+			dialogBp.setMinPackSize(new Dimension(640, 480));
+			final IInputDialog<Person> dialog = Toolkit.getActiveWindow().createChildWindow(dialogBp);
+			dialog.setVisible(true);
+			if (dialog.isOkPressed()) {
+				final Person person = dialog.getValue();
+				model.addBean(person, true);
+			}
+		}
+
+	}
+
+}
---
+++ trunk/examples/org.jowidgets.examples.common/src/main/java/org/jowidgets/examples/common/workbench/demo3/component/PersonComponentLayoutFactory.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2013, grossmann
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+package org.jowidgets.examples.common.workbench.demo3.component;
+
+import org.jowidgets.workbench.api.ILayout;
+import org.jowidgets.workbench.toolkit.api.IFolderLayoutBuilder;
+import org.jowidgets.workbench.toolkit.api.ILayoutBuilder;
+import org.jowidgets.workbench.toolkit.api.ISplitLayoutBuilder;
+import org.jowidgets.workbench.tools.FolderLayoutBuilder;
+import org.jowidgets.workbench.tools.LayoutBuilder;
+import org.jowidgets.workbench.tools.SplitLayoutBuilder;
+
+public final class PersonComponentLayoutFactory {
+
+	private static final String DEFAULT_LAYOUT_ID = "DEFAULT_LAYOUT_ID";
+	private static final String MASTER_FOLDER_ID = "MASTER_FOLDER_ID";
+	private static final String DETAIL_FOLDER_ID = "DETAIL_FOLDER_ID";
+
+	private PersonComponentLayoutFactory() {}
+
+	public static ILayout create() {
+		final ILayoutBuilder builder = new LayoutBuilder();
+		builder.setId(DEFAULT_LAYOUT_ID).setLayoutContainer(createMasterDetailSplit());
+		return builder.build();
+	}
+
+	private static ISplitLayoutBuilder createMasterDetailSplit() {
+		final ISplitLayoutBuilder result = new SplitLayoutBuilder();
+		result.setVertical().setWeight(0.5).setResizeSecond();
+		result.setFirstContainer(createMasterFolder());
+		result.setSecondContainer(createDetailFolder());
+		return result;
+	}
+
+	private static IFolderLayoutBuilder createMasterFolder() {
+		final IFolderLayoutBuilder result = new FolderLayoutBuilder(MASTER_FOLDER_ID);
+		result.setViewsCloseable(false);
+		result.addView(
+				PersonTableView.ID,
+				PersonTableView.DEFAULT_LABEL,
+				PersonTableView.DEFAULT_TOOLTIP,
+				PersonTableView.DEFAULT_ICON);
+		return result;
+	}
+
+	private static IFolderLayoutBuilder createDetailFolder() {
+		final IFolderLayoutBuilder result = new FolderLayoutBuilder(DETAIL_FOLDER_ID);
+		result.setViewsCloseable(false);
+		result.addView(
+				PersonDetailView.ID,
+				PersonDetailView.DEFAULT_LABEL,
+				PersonDetailView.DEFAULT_TOOLTIP,
+				PersonDetailView.DEFAULT_ICON);
+		return result;
+	}
+
+}
