ea74352114a13165db665ec9db12b6b5514c7193
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2013-06-03 19:54:14 +0000

    Switched default colors of levelmeter

--- trunk/core/org.jowidgets.impl/src/main/java/org/jowidgets/impl/widgets/composed/blueprint/defaults/LevelMeterDefaults.java
+++ trunk/core/org.jowidgets.impl/src/main/java/org/jowidgets/impl/widgets/composed/blueprint/defaults/LevelMeterDefaults.java
@@ -37,11 +37,11 @@ public class LevelMeterDefaults implements IDefaultInitializer<ILevelMeterSetupB
 	@Override
 	public void initialize(final ILevelMeterSetupBuilder<?> builder) {
 		builder.setLetteringVisible(false);
-		builder.setBackgroundColor(new ColorValue(192, 192, 192));
+		builder.setBackgroundColor(new ColorValue(225, 225, 225));
 		builder.setLowPeakColor(new ColorValue(49, 160, 45));
 		builder.setHighPeakColor(Colors.WARNING);
 		builder.setClipPeakColor(Colors.ERROR);
-		builder.setNoPeakColor(new ColorValue(225, 225, 225));
+		builder.setNoPeakColor(new ColorValue(192, 192, 192));
 		builder.setHighPeakThreshold(0.7d);
 		builder.setClipPeakThreshold(0.9d);
 		builder.setBoxHeight(2);
