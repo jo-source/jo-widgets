32d1508a28c38911d0b9caeae199f012fddf0aa5
herr.grossmann <herrgrossmann@users.noreply.github.com>
2013-10-15 08:55:16 +0000

    Assert supports noptEmpty for Map's now

--- modules/core/org.jowidgets.util/src/main/java/org/jowidgets/util/Assert.java
+++ modules/core/org.jowidgets.util/src/main/java/org/jowidgets/util/Assert.java
@@ -28,6 +28,7 @@
 package org.jowidgets.util;
 
 import java.util.Collection;
+import java.util.Map;
 
 public final class Assert {
 
@@ -49,6 +50,12 @@ public final class Assert {
 
 	public static void paramNotEmpty(final Collection<?> collection, final String name) {
 		if (collection == null || collection.size() <= 0) {
+			throw new IllegalArgumentException("The parameter '" + name + "' must not be empty!");
+		}
+	}
+
+	public static void paramNotEmpty(final Map<?, ?> map, final String name) {
+		if (map == null || map.size() <= 0) {
 			throw new IllegalArgumentException("The parameter '" + name + "' must not be empty!");
 		}
 	}
