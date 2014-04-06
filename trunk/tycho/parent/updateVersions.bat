46d23239a716253337b7ae3957af37be8f1fa96d
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2014-04-06 19:22:03 +0000

    updated tycho versions after release

--- trunk/tycho/parent/updateVersions.bat
+++ trunk/tycho/parent/updateVersions.bat
@@ -1,1 +1,1 @@
-mvn org.eclipse.tycho:tycho-versions-plugin:0.19.0:set-version -DnewVersion=0.31.0-SNAPSHOT -Dtycho.mode=maven
+mvn org.eclipse.tycho:tycho-versions-plugin:0.19.0:set-version -DnewVersion=0.32.0-SNAPSHOT -Dtycho.mode=maven
