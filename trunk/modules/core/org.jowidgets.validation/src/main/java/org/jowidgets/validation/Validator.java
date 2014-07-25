4854cf505f35ed3f753f08b67a809d76049bba33
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2014-07-25 15:53:37 +0000

    Added Equals and hashcode to ok validator

--- trunk/modules/core/org.jowidgets.validation/src/main/java/org/jowidgets/validation/Validator.java
+++ trunk/modules/core/org.jowidgets.validation/src/main/java/org/jowidgets/validation/Validator.java
@@ -51,9 +51,47 @@ public final class Validator {
 
 		private static final long serialVersionUID = -654830472836975532L;
 
+		private final String className = OkValidatorImpl.class.getName();
+
 		@Override
 		public IValidationResult validate(final VALUE_TYPE value) {
 			return ValidationResult.ok();
+		}
+
+		@Override
+		public String toString() {
+			return OkValidatorImpl.class.getName();
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((className == null) ? 0 : className.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(final Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (!(obj instanceof IValidator)) {
+				return false;
+			}
+			final IValidator<?> other = (IValidator<?>) obj;
+			if (className == null) {
+				if (other.getClass().getName() != null) {
+					return false;
+				}
+			}
+			else if (!className.equals(other.getClass().getName())) {
+				return false;
+			}
+			return true;
 		}
 
 	}
