f555a86a98f9e93e81f2c72996bd28c80b685692
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2015-02-05 17:19:47 +0000

    more docu

--- trunk/docu/create_docbook.bat
+++ trunk/docu/create_docbook.bat
@@ -4,6 +4,6 @@ if exist html_out/ (
 mkdir html_out
 copy style.css html_out\style.css
 xcopy images html_out\images\ 
-xsltproc --stringparam chunker.output.indent yes --stringparam l10n.gentext.language de --stringparam chunk.section.depth 4 --stringparam chunk.first.sections 1 --stringparam  section.autolabel 1 --stringparam  section.label.includes.component.label 1 --stringparam base.dir html_out\ --stringparam html.stylesheet style.css C:\docbook-xsl-1.78.1\html\chunk.xsl pandoc_out/Dokumentation.db 
+xsltproc --stringparam highlight.source 1 --stringparam chunker.output.indent yes --stringparam l10n.gentext.language de --stringparam chunk.section.depth 4 --stringparam chunk.first.sections 1 --stringparam  section.autolabel 1 --stringparam  section.label.includes.component.label 1 --stringparam base.dir html_out\ --stringparam html.stylesheet style.css C:\docbook-xsl-1.78.1\html\chunk.xsl pandoc_out/Dokumentation.db 
 
 
