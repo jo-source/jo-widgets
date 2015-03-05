6abc4cc0f9ffb5156d5efb01ac4909d836034f49
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2015-03-05 15:57:44 +0000

    Some javadoc

--- trunk/modules/core/org.jowidgets.api/src/main/java/org/jowidgets/api/model/item/ICheckedItemModel.java
+++ trunk/modules/core/org.jowidgets.api/src/main/java/org/jowidgets/api/model/item/ICheckedItemModel.java
@@ -30,12 +30,6 @@ package org.jowidgets.api.model.item;
 
 public interface ICheckedItemModel extends IToolBarItemModel, ISelectableMenuItemModel {
 
-	/**
-	 * Makes a deep copy of the item.
-	 * Registered listeners on items won't be copied.
-	 * 
-	 * @return A new instance that is a clone of this instance
-	 */
 	@Override
 	ICheckedItemModel createCopy();
 
