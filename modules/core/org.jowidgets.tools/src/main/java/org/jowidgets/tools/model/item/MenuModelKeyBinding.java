ff01d88ab89d1d2f773978104f35033eecd18874
herr.grossmann <herrgrossmann@users.noreply.github.com>
2015-03-10 10:36:40 +0000

    Enhanced MenuModelKeyBinding Utility

--- modules/core/org.jowidgets.tools/src/main/java/org/jowidgets/tools/model/item/MenuModelKeyBinding.java
+++ modules/core/org.jowidgets.tools/src/main/java/org/jowidgets/tools/model/item/MenuModelKeyBinding.java
@@ -37,14 +37,20 @@ import java.util.Map;
 import java.util.Set;
 
 import org.jowidgets.api.command.IAction;
+import org.jowidgets.api.command.IActionChangeObservable;
 import org.jowidgets.api.command.IExceptionHandler;
 import org.jowidgets.api.command.IExecutionContext;
+import org.jowidgets.api.controller.IDisposeListener;
+import org.jowidgets.api.controller.IListenerFactory;
 import org.jowidgets.api.model.IListModelListener;
 import org.jowidgets.api.model.item.IActionItemModel;
 import org.jowidgets.api.model.item.IMenuItemModel;
 import org.jowidgets.api.model.item.IMenuModel;
 import org.jowidgets.api.toolkit.Toolkit;
+import org.jowidgets.api.widgets.IComponent;
+import org.jowidgets.api.widgets.IContainer;
 import org.jowidgets.api.widgets.IWidget;
+import org.jowidgets.common.image.IImageConstant;
 import org.jowidgets.common.types.Accelerator;
 import org.jowidgets.common.types.Modifier;
 import org.jowidgets.common.types.VirtualKey;
@@ -52,10 +58,14 @@ import org.jowidgets.common.widgets.controller.IKeyEvent;
 import org.jowidgets.common.widgets.controller.IKeyListener;
 import org.jowidgets.common.widgets.controller.IKeyObservable;
 import org.jowidgets.tools.controller.KeyAdapter;
+import org.jowidgets.tools.controller.KeyObservable;
 import org.jowidgets.tools.controller.ListModelAdapter;
 import org.jowidgets.util.Assert;
 import org.jowidgets.util.ITypedKey;
 
+/**
+ * This class helps to bind all actions of a given menu to theirs accelerator keys
+ */
 public final class MenuModelKeyBinding {
 
 	private final Collection<IMenuModel> menus;
@@ -69,10 +79,62 @@ public final class MenuModelKeyBinding {
 
 	private Runnable actionsMapUpdater;
 
+	/**
+	 * Creates a new binding for a menu and a component that contains the menu.
+	 * 
+	 * If the given component is a container, a recursive key lister will be added,
+	 * otherwise a 'normal' key listener will be used for the key binding.
+	 * 
+	 * If the component will be disposed, the binding will be disposed also
+	 * 
+	 * @param menu The menu which actions should be bound
+	 * @param component The component which receives the key events
+	 */
+	public MenuModelKeyBinding(final IMenuModel menu, final IComponent component) {
+		this(Collections.singleton(menu), component);
+	}
+
+	/**
+	 * Creates a new binding for a collection of menus and a component that contains the menu.
+	 * 
+	 * If the given component is a container, a recursive key lister will be added,
+	 * otherwise a 'normal' key listener will be used for the key binding.
+	 * 
+	 * If the component will be disposed, the binding will be disposed also
+	 * 
+	 * @param menu The menu which actions should be bound
+	 * @param component The component which receives the key events
+	 */
+	public MenuModelKeyBinding(final Collection<? extends IMenuModel> menus, final IComponent component) {
+		this(menus, createRecursiveKeyObservable(component), component);
+
+		//dispose the binding if the component becomes disposed
+		component.addDisposeListener(new IDisposeListener() {
+			@Override
+			public void onDispose() {
+				dispose();
+			}
+		});
+	}
+
+	/**
+	 * Creates a new key binding for menu and a given key observable
+	 * 
+	 * @param menu The menu for the key binding
+	 * @param sourceKeyObservable The key observable that receives the key events
+	 * @param source The source widget that will be used for the execution context
+	 */
 	public MenuModelKeyBinding(final IMenuModel menu, final IKeyObservable sourceKeyObservable, final IWidget source) {
 		this(Collections.singleton(menu), sourceKeyObservable, source);
 	}
 
+	/**
+	 * Creates a new key binding for collection of menus and a given key observable
+	 * 
+	 * @param menu The menu for the key binding
+	 * @param sourceKeyObservable The key observable that receives the key events
+	 * @param source The source widget that will be used for the execution context
+	 */
 	public MenuModelKeyBinding(
 		final Collection<? extends IMenuModel> menus,
 		final IKeyObservable sourceKeyObservable,
@@ -139,6 +201,100 @@ public final class MenuModelKeyBinding {
 		updateActionsMap();
 	}
 
+	/**
+	 * Binds a collection of menus to a component
+	 * 
+	 * When bound all key events of the component (recursive, if component is a IContainer) will be
+	 * checked against the key accelerators of the menu and the actions will be performed if accelerator
+	 * matches.
+	 * 
+	 * If the component will be disposed, the binding will be disposed too
+	 * 
+	 * @param menus The menus to bind
+	 * @param component The component to bind to
+	 * 
+	 * @return The KeyBinding
+	 */
+	public static MenuModelKeyBinding bind(final Collection<? extends IMenuModel> menus, final IComponent component) {
+		return new MenuModelKeyBinding(menus, component);
+	}
+
+	/**
+	 * Binds a menu to a component
+	 * 
+	 * When bound all key events of the component (recursive, if component is a IContainer) will be
+	 * checked against the key accelerators of the menu and the actions will be performed if accelerator
+	 * matches.
+	 * 
+	 * If the component will be disposed, the binding will be disposed too
+	 * 
+	 * @param menus The menus to bind
+	 * @param component The component to bind to
+	 * 
+	 * @return The KeyBinding
+	 */
+	public static MenuModelKeyBinding bind(final IMenuModel menu, final IComponent component) {
+		return new MenuModelKeyBinding(menu, component);
+	}
+
+	private static IKeyObservable createRecursiveKeyObservable(final IComponent component) {
+		Assert.paramNotNull(component, "component");
+		final KeyObservable result = new KeyObservable();
+		final IKeyListener keyListener = new IKeyListener() {
+
+			@Override
+			public void keyReleased(final IKeyEvent event) {
+				result.fireKeyReleased(event);
+			}
+
+			@Override
+			public void keyPressed(final IKeyEvent event) {
+				result.fireKeyPressed(event);
+			}
+		};
+		if (component instanceof IContainer) {
+			addKeyListenerRecursive((IContainer) component, keyListener);
+		}
+		else {
+			addKeyListener(component, keyListener);
+		}
+		return result;
+	}
+
+	private static void addKeyListenerRecursive(final IContainer container, final IKeyListener keyListener) {
+		Assert.paramNotNull(container, "container");
+		Assert.paramNotNull(keyListener, "keyListener");
+
+		final IListenerFactory<IKeyListener> listenerFactory = new IListenerFactory<IKeyListener>() {
+			@Override
+			public IKeyListener create(final IComponent component) {
+				return keyListener;
+			}
+		};
+
+		container.addKeyListenerRecursive(listenerFactory);
+
+		container.addDisposeListener(new IDisposeListener() {
+			@Override
+			public void onDispose() {
+				container.removeKeyListenerRecursive(listenerFactory);
+			}
+		});
+	}
+
+	private static void addKeyListener(final IComponent component, final IKeyListener keyListener) {
+		Assert.paramNotNull(component, "component");
+		Assert.paramNotNull(keyListener, "keyListener");
+		component.addKeyListener(keyListener);
+
+		component.addDisposeListener(new IDisposeListener() {
+			@Override
+			public void onDispose() {
+				component.removeKeyListener(keyListener);
+			}
+		});
+	}
+
 	private static Accelerator getAccelerator(final IKeyEvent event) {
 		final VirtualKey virtualKey = event.getVirtualKey();
 		final Set<Modifier> modifier = event.getModifier();
@@ -154,11 +310,19 @@ public final class MenuModelKeyBinding {
 		return null;
 	}
 
+	/**
+	 * Adds a menu to the key binding
+	 * 
+	 * @param menu
+	 */
 	public void addMenu(final IMenuModel menu) {
 		menus.add(menu);
 		updateActionsMapLater();
 	}
 
+	/**
+	 * Disposes the key binding
+	 */
 	public void dispose() {
 		disposeActionsMap();
 		sourceKeyObservable.removeKeyListener(keyListener);
@@ -191,11 +355,18 @@ public final class MenuModelKeyBinding {
 	private void updateActionsMap(final IMenuModel menu) {
 		for (final IMenuItemModel item : menu.getChildren()) {
 			if (item instanceof IActionItemModel) {
+
 				final IAction action = ((IActionItemModel) item).getAction();
 				if (action != null) {
 					final Accelerator accelerator = action.getAccelerator();
 					if (accelerator != null) {
 						actions.put(accelerator, action);
+					}
+				}
+				else {
+					final Accelerator accelerator = item.getAccelerator();
+					if (accelerator != null) {
+						actions.put(accelerator, new ActionItemAction((IActionItemModel) item));
 					}
 				}
 			}
@@ -244,6 +415,62 @@ public final class MenuModelKeyBinding {
 		@Override
 		public IWidget getSource() {
 			return sourceWidget;
+		}
+
+	}
+
+	private final class ActionItemAction implements IAction {
+
+		private final IActionItemModel item;
+
+		public ActionItemAction(final IActionItemModel item) {
+			Assert.paramNotNull(item, "item");
+			this.item = item;
+		}
+
+		@Override
+		public String getText() {
+			return item.getText();
+		}
+
+		@Override
+		public String getToolTipText() {
+			return item.getToolTipText();
+		}
+
+		@Override
+		public IImageConstant getIcon() {
+			return item.getIcon();
+		}
+
+		@Override
+		public Character getMnemonic() {
+			return item.getMnemonic();
+		}
+
+		@Override
+		public Accelerator getAccelerator() {
+			return item.getAccelerator();
+		}
+
+		@Override
+		public boolean isEnabled() {
+			return item.isEnabled();
+		}
+
+		@Override
+		public void execute(final IExecutionContext actionEvent) throws Exception {
+			item.actionPerformed();
+		}
+
+		@Override
+		public IExceptionHandler getExceptionHandler() {
+			return null;
+		}
+
+		@Override
+		public IActionChangeObservable getActionChangeObservable() {
+			return null;
 		}
 
 	}
