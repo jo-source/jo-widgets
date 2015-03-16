ac2a30f21870d9d0cc09cd3dfe61aebd46563c7e
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2015-03-16 15:29:03 +0000

    Moved default order to interface of toolkit interceptor holder

--- trunk/modules/core/org.jowidgets.api/src/main/java/org/jowidgets/api/toolkit/IToolkitInterceptorHolder.java
+++ trunk/modules/core/org.jowidgets.api/src/main/java/org/jowidgets/api/toolkit/IToolkitInterceptorHolder.java
@@ -28,8 +28,16 @@
 
 package org.jowidgets.api.toolkit;
 
-
 public interface IToolkitInterceptorHolder {
+
+	/**
+	 * The default order for toolkit interceptors.
+	 * 
+	 * Uses values below this order to do interception before interceptors with default order.
+	 * 
+	 * Use value above this order to do interception after interceptors with default order.
+	 */
+	int DEFAULT_ORDER = 2;
 
 	IToolkitInterceptor getToolkitInterceptor();
 
