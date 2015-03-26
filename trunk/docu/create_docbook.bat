c86085786035f6a983dff2d9c7f75a07d34ab944
herr.grossmann@gmx.de <herr.grossmann@gmx.de@a5d23cbc-af31-80e9-e2db-c0456a7e813f>
2015-03-26 15:48:13 +0000

    added libs to docu to allow easy creation of docu

--- trunk/docu/create_docbook.bat
+++ trunk/docu/create_docbook.bat
@@ -10,6 +10,6 @@ copy ba_nm.pdf docu\ba_nm.pdf
 xcopy images docu\images\ 
 javac DocBookPostProcessor.java
 java DocBookPostProcessor pandoc_out/Dokumentation.db pandoc_out/Dokumentation_processed.db
-java -cp "saxon/saxon.jar;saxon/saxon65.jar;saxon/xslthl-2.1.3.jar;saxon/xercesImpl.jar;saxon/xml-apis.jar" -Dxslthl.config="file:///c:/docbook-xsl-1.78.1/highlighting/xslthl-config.xml" -Djavax.xml.parsers.DocumentBuilderFactory=org.apache.xerces.jaxp.DocumentBuilderFactoryImpl -Duser.country=DE -Duser.language=de -Djavax.xml.parsers.SAXParserFactory=org.apache.xerces.jaxp.SAXParserFactoryImpl com.icl.saxon.StyleSheet -o docu\index.html pandoc_out/Dokumentation_processed.db docbook.xsl 
+java -cp "libs/saxon/saxon.jar;libs/saxon/saxon65.jar;libs/saxon/xslthl-2.1.3.jar;libs/saxon/xercesImpl.jar;libs/saxon/xml-apis.jar" -Dxslthl.config="file:libs/docbook-xsl-1.78.1/highlighting/xslthl-config.xml" -Djavax.xml.parsers.DocumentBuilderFactory=org.apache.xerces.jaxp.DocumentBuilderFactoryImpl -Duser.country=DE -Duser.language=de -Djavax.xml.parsers.SAXParserFactory=org.apache.xerces.jaxp.SAXParserFactoryImpl com.icl.saxon.StyleSheet -o docu\index.html pandoc_out/Dokumentation_processed.db docbook.xsl 
 
 
