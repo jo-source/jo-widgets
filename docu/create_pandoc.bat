1b12c0278d03d8a683c12cd8e136f9c6cb81d8f7
herr.grossmann <herrgrossmann@users.noreply.github.com>
2015-03-27 00:01:05 +0000

    minor changes for pdf version of docu

--- docu/create_pandoc.bat
+++ docu/create_pandoc.bat
@@ -11,7 +11,7 @@ java PandocPreProcessor pandoc_in/ pandoc_in_pre/
 timeout /t 1 /nobreak > nul
 
 libs\pandoc\pandoc -s -S -t docbook pandoc_in_pre/docbook_in.md -V lang=german --toc --number-sections --epub-chapter-level=1  -o pandoc_out/Dokumentation.db
-libs\pandoc\pandoc pandoc_in_pre/pdf_in.md -V lang=german --toc --number-sections --epub-chapter-level=1 --chapters  -H latex.sty  -o pandoc_out/Dokumentation.pdf
-libs\pandoc\pandoc pandoc_in_pre/docbook_in.md -V lang=german -c style.css --toc --number-sections --epub-chapter-level=1 --chapters -o pandoc_out/Dokumentation.html
+libs\pandoc\pandoc pandoc_in_pre/pdf_in.md -V lang=german --toc --number-sections --epub-chapter-level=1 --chapters -H latex.sty --variable linkcolor=blue -o pandoc_out/Dokumentation.pdf
+libs\pandoc\pandoc pandoc_in_pre/pdf_in.md -V lang=german -c style.css --toc --number-sections --epub-chapter-level=1 --chapters -o pandoc_out/Dokumentation.html
 
 
